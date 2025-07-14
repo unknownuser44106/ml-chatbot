10b2c8271ec845b3456385c66f5fe4dfcb89d653
Raghava Reddy S <105329592+unknownuser44106@users.noreply.github.com>
2025-07-14 09:34:00 +0530

    Fixed Render deployment

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import numpy as np
 import json
 import nltk
 from nltk.stem import WordNetLemmatizer
+import os
 
 app = Flask(__name__)
 
@@ -61,5 +62,7 @@ def predict():
     return jsonify({'response': res})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    app.run(debug=False)
 
